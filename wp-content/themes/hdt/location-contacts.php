966b9076b46414d17ae4eef3a0765840c317878a
Nicholas Rowe <nick@saigon.digital>
2024-03-22 09:10:26 +0700

    comit

--- wp-content/themes/hdt/location-contacts.php
+++ wp-content/themes/hdt/location-contacts.php
@@ -1,79 +1,82 @@
 <!-- This code outputs contact details -->
 
-<?php if(get_field('display_contacts')): ?>
-  
+<?php if (get_field('display_contacts')) : ?>
 
-<?php
+
+	<?php
 
 	$namevar =  get_field('contact_name');
 	$numvar = get_field('contact_number');
 	$emailvar = get_field('contact_email_address');
 
 
-	if ($namevar || $numvar || $emailvar):?>
+	if ($namevar || $numvar || $emailvar) : ?>
 
-	<h4>Contact:</h4>
-	<p>
-	<?php if(get_field('contact_name')): ?>
-	<?php 
-	$name = get_field('contact_name');
-	echo $name;
-	?><br/>
+		<h4>Contact:</h4>
+		<p>
+			<?php if (get_field('contact_name')) : ?>
+				<?php
+				$name = get_field('contact_name');
+				echo $name;
+				?><br />
+			<?php endif; ?>
+
+
+
+			<?php if (get_field('contact_number')) : ?>
+				<?php
+				$number = get_field('contact_number');
+				echo $number;
+				?><br />
+			<?php endif; ?>
+
+
+
+			<?php if (get_field('contact_email_address')) : ?>
+				<?php
+				$email = get_field('contact_email_address');
+				?>
+				<a href="mailto:<?php echo $email; ?>">E-mail us</a><br />
+			<?php endif; ?>
+		</p>
+
 	<?php endif; ?>
 
 
-
-	<?php if(get_field('contact_number')): ?>
-	<?php 
-	$number = get_field('contact_number');
-	echo $number;
-	?><br/>
-	<?php endif; ?>
-
-
-
-	<?php if(get_field('contact_email_address')): ?>
-	<?php 
-	$email = get_field('contact_email_address');
-	?>
-	<a href="mailto:<?php echo $email; ?>">E-mail us</a><br/>
-	<?php endif; ?>
-	</p>
-
-<?php endif; ?>
-
-
-<?php
+	<?php
 
 	$web =  get_field('contact_web_address');
 	$tw = get_field('contact_twitter_page');
 	$fb = get_field('contact_facebook_page');
 
 
-	if ($web || $tw || $fb):?>
+	if ($web || $tw || $fb) : ?>
 
 
-	<h4>Connect:</h4>
-	<p>
-	<?php if(get_field('contact_web_address')): ?>
-	<?php $url = get_field('contact_web_address'); ?>
-	<a href="http://<?php echo $url; ?>" target="_blank">Visit our website</a><br/>
-	<?php endif; ?>         
+		<h4>Connect:</h4>
+		<p>
+			<?php if (get_field('contact_web_address')) : ?>
+				<?php $url = get_field('contact_web_address'); ?>
+				<a href="http://<?php echo $url; ?>" target="_blank">Visit our website</a><br />
+			<?php endif; ?>
 
 
-	<?php if(get_field('contact_facebook_page')): ?>
-	<?php $facebook = get_field('contact_facebook_page'); ?>
-	<a href="http://<?php echo $facebook; ?>" target="_blank"class="social-link"><img src="<?php echo get_site_url(); ?>/wp-content/themes/hdt/assets/img/facebook-icon.png"/></a>
-	 <?php endif; ?>
+			<?php if (get_field('contact_facebook_page')) : ?>
+				<?php $facebook = get_field('contact_facebook_page'); ?>
+				<a href="http://<?php echo $facebook; ?>" target="_blank" class="social-link"><img src="<?php echo get_site_url(); ?>/wp-content/themes/hdt/assets/img/facebook-icon.png" /></a>
+			<?php endif; ?>
 
 
-	<?php if(get_field('contact_twitter_page')): ?>
-	<?php $twitter = get_field('contact_twitter_page'); ?>
-	<a href="http://<?php echo $twitter; ?>" target="_blank" class="social-link"><img src="<?php echo get_site_url(); ?>/wp-content/themes/hdt/assets/img/twitter-icon.png"/></a>
+			<?php if (get_field('contact_twitter_page')) : ?>
+				<?php $twitter = get_field('contact_twitter_page'); ?>
+				<a href="http://<?php echo $twitter; ?>" target="_blank" class="social-link"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/x-logo.png" width="40" height="40" alt="x-logo" /></a>
+			<?php endif; ?>
+
+
+
+
+		</p>
+
 	<?php endif; ?>
-
-	</p>
-
-<?php endif; ?>
 
 <?php endif; ?>
--- wp-content/themes/hdt/single-location.php
+++ wp-content/themes/hdt/single-location.php
@@ -1,16 +1,16 @@
-<?php 
+<?php
 
 get_header(); ?>
 
 <script type="text/javascript">
-    $(document).ready(function() {
+  $(document).ready(function() {
     $(".em-locations-list li:nth-child(2n+2)").addClass("position-two");
-    });
+  });
 </script>
 
 <div class="row">
 
-    <div class="main-content">
+  <div class="main-content">
 
     <div class="large-12 column">
 
@@ -21,102 +21,102 @@ get_header(); ?>
       <aside class="main-sidebar contact-details">
 
         <div style="margin-bottom:20px;">
-            <?php echo the_post_thumbnail(); ?>
+          <?php echo the_post_thumbnail(); ?>
         </div>
 
-         <h3>Contact Us</h3>
-         <h4>Address:</h4>
-         <p>
-         <span style="display:block"><?php echo $EM_Location->output('#_LOCATIONREGION'); ?></span>
-         <span style="display:block"><?php echo $EM_Location->output('#_LOCATIONADDRESS'); ?></span>
-         <span style="display:block"><?php echo $EM_Location->output('#_LOCATIONTOWN'); ?></span>
+        <h3>Contact Us</h3>
+        <h4>Address:</h4>
+        <p>
+          <span style="display:block"><?php echo $EM_Location->output('#_LOCATIONREGION'); ?></span>
+          <span style="display:block"><?php echo $EM_Location->output('#_LOCATIONADDRESS'); ?></span>
+          <span style="display:block"><?php echo $EM_Location->output('#_LOCATIONTOWN'); ?></span>
 
-         <span style="display:block"><?php echo $EM_Location->output('#_LOCATIONPOSTCODE'); ?></span>
-         </p>
+          <span style="display:block"><?php echo $EM_Location->output('#_LOCATIONPOSTCODE'); ?></span>
+        </p>
 
-         <?php get_template_part( 'location-contacts' ); ?>
-                    
+        <?php get_template_part('location-contacts'); ?>
+
       </aside>
 
     </div>
 
     <div class="large-9 columns">
 
-      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-        <div class="title-strip">
+          <div class="title-strip">
 
-            <?php if ( function_exists('yoast_breadcrumb') ) {
-                yoast_breadcrumb('<p id="breadcrumbs">','</p>');
+            <?php if (function_exists('yoast_breadcrumb')) {
+              yoast_breadcrumb('<p id="breadcrumbs">', '</p>');
             } ?>
 
-        </div>
+          </div>
 
-        <article class="post-content" >
+          <article class="post-content">
 
-          <h2 class="page-title"><?php the_title(); ?></h2>
+            <h2 class="page-title"><?php the_title(); ?></h2>
 
-          <div class="general-location-wrapper-no-padding">
+            <div class="general-location-wrapper-no-padding">
               <?php the_content(); ?>
-          </div>
-
-              
-          <?php if(get_field('event_host')): ?>
-           <div class="general-location-wrapper events-listings">
-           <h3>What's On</h3>
-
-          <?php 
-          //Checks if the custom field Group Link has a value assigned
-          if(get_field('group_link')) {
-
-          //Gets the tag name
-          $term = get_field('group_link');
-          $term_name = $term->name;
+            </div>
 
 
-          //Adds the tag name to the shortcode
-          $group_link = '[events_list tag='.'"'.$term_name.'"'.' limit=5 scope="today-tomorrow" pagination=1]';
-          //Echos the shortcode, therefore showing events 
+            <?php if (get_field('event_host')) : ?>
+              <div class="general-location-wrapper events-listings">
+                <h3>What's On</h3>
 
-          echo do_shortcode($group_link);
+                <?php
+                //Checks if the custom field Group Link has a value assigned
+                if (get_field('group_link')) {
 
-          } else {
-            //Get the current location ID
-
-          echo $location_id;
-
-          $location_id = $EM_Location->output('#_LOCATIONID');
-
-          //Adds the tag name to the shortcode
-          $event_list = '[events_list location='.'"'.$location_id.'"'.' limit=5 scope="today-tomorrow"  pagination=1]';
-          //Echos the shortcode, therefore showing events 
-
-          echo do_shortcode($event_list);
-          }
-
-          ?>
-          </div>
-          <?php endif; ?>
+                  //Gets the tag name
+                  $term = get_field('group_link');
+                  $term_name = $term->name;
 
 
-          <?php get_template_part( 'location-news' ); ?>
+                  //Adds the tag name to the shortcode
+                  $group_link = '[events_list tag=' . '"' . $term_name . '"' . ' limit=5 scope="today-tomorrow" pagination=1]';
+                  //Echos the shortcode, therefore showing events 
 
-          <?php get_template_part( 'location-downloads' ); ?>
+                  echo do_shortcode($group_link);
+                } else {
+                  //Get the current location ID
 
-          <?php get_template_part( 'location-gallery' ); ?>
+                  echo $location_id;
+
+                  $location_id = $EM_Location->output('#_LOCATIONID');
+
+                  //Adds the tag name to the shortcode
+                  $event_list = '[events_list location=' . '"' . $location_id . '"' . ' limit=5 scope="today-tomorrow"  pagination=1]';
+                  //Echos the shortcode, therefore showing events 
+
+                  echo do_shortcode($event_list);
+                }
+
+                ?>
+              </div>
+            <?php endif; ?>
 
 
-          <div class="general-location-wrapper general-location-wrapper-no-border">
-           <h3>How To Find Us</h3>
-          <?php echo $EM_Location->output('#_LOCATIONMAP'); ?>
-          </div>
+            <?php get_template_part('location-news'); ?>
 
-          <?php endwhile; else: ?>
-            <p><?php _e('Sorry, no posts matched yourq criteria.'); ?></p>
-          <?php endif; ?>
+            <?php get_template_part('location-downloads'); ?>
 
-        </article>
+            <?php get_template_part('location-gallery'); ?>
+
+
+            <div class="general-location-wrapper general-location-wrapper-no-border">
+              <h3>How To Find Us</h3>
+              <?php echo $EM_Location->output('#_LOCATIONMAP'); ?>
+            </div>
+
+          <?php endwhile;
+      else : ?>
+          <p><?php _e('Sorry, no posts matched yourq criteria.'); ?></p>
+        <?php endif; ?>
+
+          </article>
 
     </div>
-    
-<?php get_footer(); ?>
+
+    <?php get_footer(); ?>
